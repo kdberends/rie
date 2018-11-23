4bf3d044873679943e5f3a685b21a77d0289b60d
kdberends <k.d.berends@utwente.nl>
2018-11-23 18:23:21 +0100

    added some apple stuff for pwa

--- js/explore.js
+++ js/explore.js
@@ -3,8 +3,26 @@
  * 
  * 
  */////////////////////////////////////////////////////////////
-const version = "0.2.1";
+const version = "0.2.3";
 
+
+/*
+https://stackoverflow.com/questions/50543163/can-i-detect-if-my-pwa-is-launched-as-an-app-or-visited-as-a-website
+// Detects if device is on iOS 
+const isIos = () => {
+  const userAgent = window.navigator.userAgent.toLowerCase();
+  return /iphone|ipad|ipod/.test( userAgent );
+};
+// Detects if device is in standalone mode
+const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.navigator.standalone);
+
+// Checks if should display install popup notification:
+if (isIos() && !isInStandaloneMode()) {
+  this.setState({ showInstallMessage: true });
+};
+
+this.setState({ showInstallMessage: true });
+*/
 
 /** ////////////////////////////////////////////////////////////
  * Extend jquery

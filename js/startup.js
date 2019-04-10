8f3ac928ec5e2787325e786a7830cbd2596b0b50
Koen Berends <k.d.berends@utwente.nl>
2019-04-10 14:18:22 +0200

    #34 button for phone moved to side of screen

--- js/startup.js
+++ js/startup.js
@@ -1,8 +1,10 @@
 // call functions at start
 $(window).on('load', function() {
     // your code here
+    try{
     console.log('Opening story overview')
     openStoryOverview();
 	toggleApp(0);
-	$('#loader-wrapper').addClass('loaded');
+	$('#loader-wrapper').addClass('loaded');}
+	catch(e) {console.log("unsupported browser")}
 });
